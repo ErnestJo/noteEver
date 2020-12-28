77d1a08bd80a8c5ae176b0d6fa33a3a80bad56b9
Ernest Joseph Nzallawahe <smartlabstech@yahoo.com>
2020-12-28 11:38:07 +0300

    added directories

---
+++ src/fire.js
@@ -0,0 +1,16 @@
+
+  import firebase from 'firebase'
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDyatTO5qxVzfrP8ZmxL5MG28vawWPLhtM",
+    authDomain: "evernote-app-3a316.firebaseapp.com",
+    projectId: "evernote-app-3a316",
+    storageBucket: "evernote-app-3a316.appspot.com",
+    messagingSenderId: "657012445346",
+    appId: "1:657012445346:web:20dd4c9c5ca242b8c787fb",
+  };
+  const fire = firebase.initializeApp(firebaseConfig);
+
+
+  export default fire;
